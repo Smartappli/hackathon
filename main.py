dd8a95cd9e3aa9315acb41764a9bf7af40839ad5
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-01-20 12:15:14 +0100

    write_results_csv

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 # Discord:
 
 import os
-
+import spacy
+from utils.reader_writer import read_results_file
 from utils.file_operation import create_or_recover_vocabulary, number_of_patients_and_files
 
 # Parameters
@@ -14,28 +15,58 @@ verbose = True
 
 vocabulary_file = "vocabulary.csv"
 vocabulary_file_path = os.path.join(os.getcwd(), vocabulary_file)
-patients_directory_name = 'patients' #  Répertoire contenant l'ensemble des répertoires patient
-repertoire_patients = os.path.join(os.getcwd(), patients_directory_name)
+results_file = "results.csv"
+results_file_path = os.path.join(os.getcwd(), results_file)
+patients_directory_name = 'patients'
+patients_root_directory = os.path.join(os.getcwd(), patients_directory_name)
 
 # Functions Definition
+nlp = spacy.load('fr_core_news_sm', disable=['ner', 'textcat'])
 
+# Main
+print("Step 1 - Vocabulary File Reading or Creation.")
 
-
-if verbose == True:
-    print("Step 1 - Vocabulary File Reading or Creation.")
+if verbose:
     print(f"-----> Vocabulary file path: {vocabulary_file_path}")
-    vocabulary = create_or_recover_vocabulary(vocabulary_file_path)
+
+vocabulary = create_or_recover_vocabulary(vocabulary_file_path)
+
+if verbose:
     if len(vocabulary) > 0:
         print("-----> The vocabulary file has been found.")
         print(f"-----> Vocabulary Set: {vocabulary}")
     else:
         print(" -----> The dictionary file does not exist.")
 
-    print("\nStep 2 - Counting the number of files to be processed.")
-    print(f"-----> Patients Directory: {repertoire_patients}")
-    total_directories, total_files = number_of_patients_and_files(repertoire_patients)
+print("\nStep 2 - Counting the number of files to be processed.")
+
+if verbose:
+    print(f"-----> Patients Directory: {patients_root_directory}")
+
+total_directories, total_files = number_of_patients_and_files(patients_root_directory)
+
+if verbose:
     print(f"-----> The number of patients to be treated: {total_directories}")
     print(f"-----> The number of files to be processed: {total_files}")
 
-    print("\nStep 3 - Traitement des fichiers des patients.")
+print("\nStep 3 - Step 1 - Results File Reading or Creation.")
+
+if verbose:
+    print(f"-----> Results file path: {results_file_path}")
+
+    results = read_results_file(results_file_path)
+
+if verbose:
+    if len(results) > 0:
+        print("-----> The vocabulary file has been found.")
+        print(f"-----> Results Set: {results}")
+    else:
+        print("-----> The dictionary file does not exist.")
+
+print("\nStep 4 - Identification of patient directories to be processed")
+patients_path = [os.path.join(patients_root_directory, patient) for patient in os.listdir(patients_root_directory) if os.path.isdir(os.path.join(patients_root_directory, patient))]
+if verbose:
+    print (f"-----> Patient directories to be processed: {patients_path}")
+
+print("\nStep 5 - Patients Processing")
 
--- utils/reader_writer.py
+++ utils/reader_writer.py
@@ -1,3 +1,4 @@
+import os
 import csv
 
 
@@ -21,3 +22,52 @@ def read_vocabulary_file(file_path):
             vocabulary.append(word)
 
     return vocabulary
+
+
+def read_results_file(file_path):
+    """
+    Load single-token sets per patient from a CSV file.
+
+    Parameters:
+    - file_path (str): The path to the CSV file containing patient and token data.
+
+    Returns:
+    - dict: A dictionary where keys are patient identifiers and values are sets
+            containing existing single tokens for each patient.
+            Example: {'patient1': {'token1', 'token2'}, 'patient2': {'token3', 'token4'}, ...}
+
+    Note:
+    - The CSV file should have two columns: 'patient' and 'existing_tokens'.
+    - 'existing_tokens' should be a comma-separated string of tokens.
+    - If the file does not exist, an empty dictionary is returned.
+    """
+    single_tokens_sets_per_patient = {}
+    file_exists = os.path.exists(file_path)
+    if file_exists:
+        with open(file_path, 'r', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                patient, existing_tokens = row
+                single_tokens_sets_per_patient[patient] = set(existing_tokens.split(', '))
+
+    return single_tokens_sets_per_patient
+
+
+def write_results_csv(file_path, single_tokens_sets_per_patient):
+    """
+    Write results to a CSV file.
+
+    Parameters:
+    - file_path (str): The path to the CSV file to be created or overwritten.
+    - single_tokens_sets_per_patient (dict): A dictionary where keys are patient identifiers and
+                                             values are sets containing unique tokens for each patient.
+
+    Note:
+    - The CSV file will have two columns: 'Patient' and 'Tokens Uniques'.
+    - 'Tokens Uniques' will be a comma-separated string of unique tokens for each patient.
+    """
+    with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Patient', 'Tokens Uniques'])
+        for patient, ensemble_tokens_uniques in single_tokens_sets_per_patient.items():
+            writer.writerow([patient, ', '.join(single_tokens_sets_per_patient)])
