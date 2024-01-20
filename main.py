cde7983a19c1e6c47d761bae50f05f8f6cad41ba
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-01-20 11:04:47 +0100

    create_or_recover_vocabulary

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 # Discord:
 
 import os
-from utils.reader_writer import read_vocabulary_file
-from utils.file_operation import number_of_patients_and_files
+
+from utils.file_operation import create_or_recover_vocabulary, number_of_patients_and_files
 
 # Parameters
 
@@ -18,42 +18,24 @@ patients_directory_name = 'patients' #  Répertoire contenant l'ensemble des ré
 repertoire_patients = os.path.join(os.getcwd(), patients_directory_name)
 
 # Functions Definition
-def create_or_recover_vocabulary(file_path):
-    """
-    Creates or retrieves the vocabulary list from a CSV file.
 
-    Parameters:
-    - file_path (str): The path to the CSV file.
-    - verbose (bool, optional): Indicates whether to display detailed messages. Defaults to False.
-
-    Returns:
-    - set: A set containing the words in the CSV file, or an empty set if the file doesn't exist.
-    """
-    if os.path.exists(file_path):
-        # The file exists, process and create the vocabulary list
-        if verbose:
-            print("-----> The vocabulary file has been found.")
-
-        vocabulary_list = read_vocabulary_file(file_path)
-    else:
-        # File does not exist, create an empty list
-        if verbose:
-            print(" -----> The dictionary file does not exist.")
-
-        vocabulary_list = []
-
-    return set(vocabulary_list)
 
 
 if verbose == True:
     print("Step 1 - Vocabulary File Reading or Creation.")
     print(f"-----> Vocabulary file path: {vocabulary_file_path}")
     vocabulary = create_or_recover_vocabulary(vocabulary_file_path)
-    print(f"-----> Vocabulary Set: {vocabulary}")
+    if len(vocabulary) > 0:
+        print("-----> The vocabulary file has been found.")
+        print(f"-----> Vocabulary Set: {vocabulary}")
+    else:
+        print(" -----> The dictionary file does not exist.")
 
-    print ("Step 2 - Counting the number of files to be processed.")
+    print("\nStep 2 - Counting the number of files to be processed.")
     print(f"-----> Patients Directory: {repertoire_patients}")
     total_directories, total_files = number_of_patients_and_files(repertoire_patients)
     print(f"-----> The number of patients to be treated: {total_directories}")
     print(f"-----> The number of files to be processed: {total_files}")
+
+    print("\nStep 3 - Traitement des fichiers des patients.")
 
--- utils/file_operation.py
+++ utils/file_operation.py
@@ -1,7 +1,38 @@
 import os
+from utils.reader_writer import read_vocabulary_file
 
+def create_or_recover_vocabulary(file_path):
+    """
+    Creates or retrieves the vocabulary list from a CSV file.
+
+    Parameters:
+    - file_path (str): The path to the CSV file.
+    - verbose (bool, optional): Indicates whether to display detailed messages. Defaults to False.
+
+    Returns:
+    - set: A set containing the words in the CSV file, or an empty set if the file doesn't exist.
+    """
+    if os.path.exists(file_path):
+        # The file exists, process and create the vocabulary list
+        vocabulary_list = read_vocabulary_file(file_path)
+    else:
+        # File does not exist, create an empty list
+        vocabulary_list = []
+
+    return set(vocabulary_list)
 
 def number_of_patients_and_files(root_directory):
+    """
+    Count the number of patient directories and total files within those directories.
+
+    Parameters:
+    - root_directory (str): The root directory containing patient subdirectories.
+
+    Returns:
+    - tuple: A tuple containing two values:
+        - total_directories (int): The total number of patient directories.
+        - total_files (int): The total number of files across all patient directories.
+    """
 
     total_files = 0
     total_directories = 0
